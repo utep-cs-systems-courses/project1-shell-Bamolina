cb623bd0be397a42392d3d34422ce801631c1a5a
root <root@LAPTOP-RT4P0UFN.localdomain>
2020-09-12 20:30:52 -0600

    Added fork functionality. Reqs 1, 2, 4

--- shell/shell.py
+++ shell/shell.py
@@ -12,7 +12,7 @@ def main():
         else:
             os.write(1, (" $ ").encode())
 
-        input = os.read(0,1000)
+        input = os.read(0, 1000) #arbitrary number
         input = input.decode().split()
 
         userInputHandler(input)
@@ -23,7 +23,7 @@ def userInputHandler(input):
     if len(input) == 0:
         return
     if input[0].lower() == "exit":
-        return;
+        sys.exit(0)
     elif input[0].lower() == "cd":
         if len(input) > 1:
             try:
@@ -32,6 +32,22 @@ def userInputHandler(input):
                 pass
         else:
             os.chdir("..")
+    else: #taken from p3 demo
+        pid = os.getpid()
+        rc = os.fork()
+        if rc < 0:
+            os.write(2, ("fork failed, returning %d\n" % rc).encode())
+            sys.exit(1)
+        elif rc == 0:
+            for dir in re.split(":", os.environ['PATH']):  # try each directory in the path
+                program = "%s/%s" % (dir, input[0])
+                try:
+                    os.execve(program, input, os.environ)  # try to exec program
+                except FileNotFoundError:  # ...expected
+                    pass  # ...fail quietly
+
+            os.write(2, ("Child:    Could not exec %s\n" % input[0]).encode())
+            sys.exit(1)  # terminate with error
 
 
 if __name__ == "__main__":
