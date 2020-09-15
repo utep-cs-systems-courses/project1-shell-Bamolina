ff7cd0c2ce81b3d1a119429b75070cc3fefc4b9f
root <root@LAPTOP-RT4P0UFN.localdomain>
2020-09-15 15:15:03 -0600

    Created pipeHandler, separated functionality of inputHandler into commandHandler. Added pipe functionality

--- shell/shell.py
+++ shell/shell.py
@@ -33,25 +33,59 @@ def userInputHandler(input):
                 pass
         else:
             os.chdir("..")
-    else:  # taken from p3 demo
-        pid = os.getpid()
-        rc = os.fork()
-        if rc < 0:
-            os.write(2, ("fork failed, returning %d\n" % rc).encode())
-            sys.exit(1)
-        elif rc == 0:
-            for dir in re.split(":", os.environ['PATH']):  # try each directory in the path
-                program = "%s/%s" % (dir, input[0])
-                try:
-                    os.execve(program, input, os.environ)  # try to exec program
-                except FileNotFoundError:  # ...expected
-                    pass  # ...fail quietly
+    elif '|' in input: # If there is a pipe in the input, pass the input to the pipeHandler
+        pipeHandler(input)
+    else:
+        commandHandler(input)
 
-            os.write(2, ("Command not found %s\n" % input[0]).encode())
-            sys.exit(1)  # terminate with error
-        else:
-            os.wait()  # fixes output string timing
+def pipeHandler(input):
+    leftInput  = input[0:input.index('|')]
+    rightInput = input[input.index('|')+1:]
+    # from p5 demo
+    pr, pw = os.pipe()
+    rc = os.fork()
+    if rc < 0:
+        sys.exit(1)
 
+    elif rc == 0:  # child - will write to pipe
+        os.close(1)  # redirect child's stdout
+        os.dup(pw)
+        os.set_inheritable(1, True)
+        for fd in (pr, pw):
+            os.close(fd)
+        commandHandler(leftInput)
+        sys.exit(1)
+
+    else:  # parent (forked ok)
+        os.close(0)
+        os.dup(pr)
+        os.set_inheritable(0, True)
+        for fd in (pw, pr):
+            os.close(fd)
+        if '|' in rightInput: # Recursive call in case there are multiple pipes
+            pipeHandler(rightInput)
+        commandHandler(rightInput)
+        sys.exit(1)
+
+def commandHandler(input):
+    # taken from p3 demo
+    pid = os.getpid()
+    rc = os.fork()
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+    elif rc == 0:
+        for dir in re.split(":", os.environ['PATH']):  # try each directory in the path
+            program = "%s/%s" % (dir, input[0])
+            try:
+                os.execve(program, input, os.environ)  # try to exec program
+            except FileNotFoundError:  # ...expected
+                pass  # ...fail quietly
+
+        os.write(2, ("Command not found: %s\n" % input[0]).encode())
+        sys.exit(1)  # terminate with error
+    else:
+        os.wait()  # fixes output string timing
 
 if __name__ == "__main__":
     main()
